6b21ad0c075562261af137d47b36943a87dcbb5f
perezmarianopm <perezmariano.pm@gmail.com>
2023-06-12 17:33:28 -0300

    changes products

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,64 +4,179 @@ const products = [
         type: "burguer",
         name: "Sandwich de Pollo Frito 'Crocken'", 
         description: "Pan de papa , mayonesa Crocken ahumada, pechuga Crocken , lechuga, tomate y cebolla colorada encurtida.",
-        price: 2119,
+        price: 2669,
         img: "burguer-crocken.jpg",
         cantidad: 1,
-        title: "nuevo"
+        title: "nuevo",
+        stars: 4.5
     },
     {   
         id: 2,
         type: "burguer",
         name: "Sandwich de Pollo Frito 'Hot Spicy'", 
         description: "Pan de papa, mayonesa Crocken picante, pechuga Crocken y ajies en conserva.",
-        price: 2139,
+        price: 2709,
         img: "burguer-hot-spicy.jpg",
         cantidad: 1,
-        title: "nuevo"
-
+        title: "nuevo",
+        stars: 4.4
     },
     {
         id: 3,
         type: "burguer",
         name: "Sandwich de Pollo Frito 'Crunchy'", 
         description: "Pan de papa, mayonesa Crocken picante, pechuga Crocken y ajies en conserva.",
-        price: 2169,
+        price: 2769,
         img: "burguer-crunchy.jpg",
         cantidad: 1,
-        title: "nuevo"
-
+        title: "nuevo",
+        stars: 4.8
     },
     {
         id: 4,
         type: "burguer",
         name: "Sandwich de Pollo Frito 'Original'", 
         description: "Pan de papa, mayonesa Crocken picante, pechuga Crocken y ajies en conserva.",
-        price: 2229,
+        price: 2789,
         img: "burguer-original.jpg",
         cantidad: 1,
-        title: "nuevo"
-
+        title: "nuevo",
+        stars: 4.5
     },
     {
         id: 5,                                                        
         type: "burguer",
         name: "Sandwich de Pollo Frito 'De la Casa'", 
         description: "Pan de papa, mayonesa Crocken picante, pechuga Crocken y ajies en conserva.",
-        price: 2269,
+        price: 2829,
         img: "burguer-de-la-casa.jpg",
         cantidad: 1,
-        title: "nuevo"
-
+        title: "nuevo",
+        stars: 4.5
     },
     {
         id: 6,
         type: "burguer",
         name: "Sandwich de Pollo Frito 'Americano'", 
         description: "Pan de papa, mayonesa Crocken picante, pechuga Crocken y ajies en conserva.",
-        price: 2379,
+        price: 2969,
         img: "burguer-americano.jpg",
         cantidad: 1,
-        title: "la mas caliente"
-
+        title: "la mas caliente",
+        stars: 4.5
+    },
+    {
+        id: 7,
+        type: "burguer",
+        name: "Sandwich Veggie", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2629,
+        img: "burguer-veggie.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 8,
+        type: "combo-1",
+        name: "Combo Crocken Mini", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-crocken-mini.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 9,
+        type: "combo-1",
+        name: "Combo Crocken Junior", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-crocken-jr.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 10,
+        type: "combo-1",
+        name: "Combo Crocken Medium", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-crocken-medium.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 11,
+        type: "combo-1",
+        name: "Combo Crocken Senior", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-crocken-senior.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 12,
+        type: "combo-1",
+        name: "Combo de 3 Alitas Crocken", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-3alitas-crunch.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 13,
+        type: "combo-1",
+        name: "Combo de 4 Alitas Crocken", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-4alitas-crunch.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 14,
+        type: "combo-1",
+        name: "Combo de 5 Alitas Crocken", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-5alitas-crunch.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 15,
+        type: "combo-1",
+        name: "Pop´s Crocken", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "pops-crocken.jpg",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
+    },
+    {
+        id: 16,
+        type: "combo-1",
+        name: "Pop´s Crocken", 
+        description: "Pan de papa, mayonesa Crocken verde, rúcula, tomate, coleslaw, cebolla morada encurtida y queso tybo.",
+        price: 2649,
+        img: "combo-pechugas-crocken.webp",
+        cantidad: 1,
+        title: "nuevo",
+        stars: 4.5
     },
 ]
+
+const appState = {
+    activeFilter: 'burguer',
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,40 @@
 // Elementos DOM
+// Iconos
 const iconCart = document.querySelector(".cart-icon")
 const iconMenu = document.querySelector(".icon-menu")
 const iconCloseMenu = document.querySelector(".icon-close-menu")
 const iconCloseCart = document.querySelector(".icon-close-cart")
-const cart = document.querySelector(".cart")
+
+// Categorias
+const categoriesContainer = document.querySelector(".categories")
+const categoriesList = document.querySelectorAll(".category")
+
+// Carrito
+const cartCount = document.querySelector(".cart-count") 
+const cartMenu = document.querySelector(".cart")
+const productsCart = document.querySelector(".cart-container")
+const cartTotal = document.querySelector("#total-cart")
+const buyBtn = document.querySelector(".btn-buy")
+
+
 const navbar = document.querySelector(".navbar")
 const menu = document.querySelector(".navbar-responsive")
 const overlay = document.querySelector(".overlay")
-const productsContainer = document.querySelector("#products-container")
+
+// Productos
+const productsContainer = document.querySelector(".products-container")
+let productModal = ''
 const boxes = document.querySelectorAll(".box")
+
+//  Modal
+const successModal = document.querySelector(".add-modal")
+
+// LocalStorage
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+const saveCart = () =>{
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
 
 const animationBoxes = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -35,41 +61,48 @@ const scroll = () => {
     }
 }
 
-const renderProducts = () => {
-    products.forEach((prod) => {
-        const {id, name, description, price, img, cant, title} = prod 
-        productsContainer.innerHTML += 
-        `
-        <li class="splide__slide">
-            <div class="product">
-                <div class="product-img">
-                    <img src="./assets/img/products/${img}" alt="" srcset="">
-                    <p>${description}</p>
+// Crear estructura HTML de las Cards
+const createProductTemplate = (product) => {
+    const {id, name, description, price, img, cant, title, stars} = product
+    return `
+        <div class="product">
+            <div class="product-modal" data-id="${id}"></div>
+            <div class="product-img">
+                <img src="./assets/img/products/${img}" alt="" srcset="">
+                <div class="product-stars">
+                    <i class="fa-solid fa-star star"></i>
+                    <p class="stars-number">${stars}</p>
                 </div>
-                <div class="product-info">
-                    <div class="product-title" >
-                        <span class="new">¡${title}!</span>
-                        <h3 class="title">${name}</h3>
-                    </div>
-                    <div class="product-description">
-                        <span class="product-price">$${price}</span>
-                        <a href="" class="btn btn-orange product-add">
-                            añadir
-                            <i class="fa-solid fa-cart-shopping"></i>
-                        </a>
-                    </div>
-                </div>
-                
-                
+                <p class="product-ingredients">${description}</p>
             </div>
-        </li>
+            <div class="product-info">
+                <div class="product-title">
+                    <span class="new">¡${title}!</span>
+                    <h3 class="title">${name}</h3>
+                </div>
+                <div class="product-description">
+                    <span class="product-price">$${price}</span>
+                    <button class="btn-container">
+                        <i class="btn-add btn-orange btn-circle fa-solid fa-plus" data-id="${id}"
+                        data-name="${name}"
+                        data-price="${price}"
+                        data-img="${img}"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
         `
-    })
+        
+}
+
+// Renderizar productos
+const renderProducts = (productsList) => {
+    productsContainer.innerHTML =  productsList.map(createProductTemplate).join("")
     
 }
 
 const toggleCart = () => {
-	cart.classList.toggle("open-cart");
+	cartMenu.classList.toggle("open-cart");
 	overlay.classList.toggle("show-overlay");
 };
 
@@ -79,13 +112,13 @@ const toggleMenu = () => {
 };
 
 const closeOnScroll = () => {
-	cart.classList.remove("open-cart");
+	cartMenu.classList.remove("open-cart");
     menu.classList.remove("open-menu");
 	overlay.classList.remove("show-overlay");
 };
 
 const closeOnOverlayClick = () => {
-    cart.classList.remove("open-cart");
+    cartMenu.classList.remove("open-cart");
     menu.classList.remove("open-menu");
 	overlay.classList.remove("show-overlay");
 };
@@ -102,11 +135,256 @@ const verificarTamañoPantalla = () => {
     }
 }
 
+// Funcion que pregunta por el filtro (Validacion)
+const isInactiveFilterBtn = (element) => {
+    return (
+        element.classList.contains("category") &&
+        !element.classList.contains("active")
+    )
+}
 
+// Funcion que cambia el estilo del boton al filtrar
+const changeBtnActiveState = (selectedCategory) => {
+    const categories = [...categoriesList]
+    categories.forEach((category) => {
+        if (category.dataset.category !== selectedCategory) {
+            category.classList.remove("active")
+            return
+        }
+        category.classList.add("active")
+    })
+}
+
+// Funcion que cambia el filtro
+const changeFilterState = (btn) => {
+    appState.activeFilter =  btn.dataset.category  
+    changeBtnActiveState(appState.activeFilter)  
+}
+
+// Renderiza los productos filtrados
+const renderFilteredProducts = () => {
+    const filteredProducts = products.filter((product) => {
+        return product.type === appState.activeFilter
+    })
+    renderProducts(filteredProducts)
+}
+
+// Aplicar filtros
+const applyFilter = ({target}) => {
+    // Chequear que sea boton y no esté activo
+    if (!isInactiveFilterBtn(target)) {
+        return
+    }
+    // Cambiar el estdo del filtro
+    changeFilterState(target)
+
+    // Si hay filtro activo, renderizo productos
+    productsContainer.innerHTML = ''
+    if (appState.activeFilter) {
+        renderFilteredProducts()
+        return
+    }
+}
+
+// CARRITO
+
+const createCartProductTemplate = (cartProduct) => {
+    const {id, name, price, img, cantidad} = cartProduct
+    return `
+    <div class="cart-item">
+        <div class="item-img">
+            <img src="./assets/img/products/${img}" />
+        </div>
+        <div class="item-info">
+            <div class="item-info-text">
+                <h3 class="item-title">${name}</h3>
+                <span class="item-description">+ Papas Fritas (sin cargo)</span>
+                <span class="item-price">$${price}</span>
+            </div>
+            <div class="item-handler">
+                <i class="item-edit fa-solid fa-pen-to-square"></i>
+                <span class="quantity-handler down" data-id="${id}">-</span>
+                <span class="item-quantity">${cantidad}</span>
+                <span class="quantity-handler up" data-id="${id}">+</span>
+                <i class="item-trash fa-solid fa-trash-can" data-id="${id}"></i>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+const renderCart = () => {
+    if (!cart.length) {
+        productsCart.innerHTML = `<p class="none-products" >No hay productos en el carrito.</p>`
+        return
+    }
+    productsCart.innerHTML = cart.map(createCartProductTemplate).join("")
+}
+
+const getCartTotal = () => {
+    return cart.reduce((acc, val) => {
+        return acc + Number(val.price) * val.cantidad
+    }, 0)
+}
+
+const showCartTotal = () => {
+    cartTotal.innerHTML = `$${getCartTotal().toFixed(2)}`
+}
+
+const createProductData = (product) => {
+    const {id, name, price, img} = product
+    return {id, name, price, img}
+}
+
+const isExistingCartProduct = (product) => {
+    return cart.find((item) => {
+        return item.id === product.id
+    })
+}
+
+const addUnitToProduct = (product) => {
+    cart = cart.map((cartProduct) => {
+        return cartProduct.id === product.id
+            ? {...cartProduct, cantidad: cartProduct.cantidad + 1}
+            : cartProduct
+    })
+}   
+
+const showSuccessModal = (msg, id) => {
+    console.log(productModal)
+    productModal.forEach((product) => {
+        console.log(product.dataset.id)
+        if (product.dataset.id === id) {
+            product.classList.add("active-modal")
+            product.textContent = msg
+            setTimeout(() => {
+                product.classList.remove("active-modal")
+            }, 1500)
+        }
+    })
+}
+
+const createCartProduct = (product) => {
+    cart = [
+        ...cart,
+        {
+            ...product,
+            cantidad: 1,
+        },
+    ]
+}
+
+const disableButton = (btn) => {
+    if (!cart.length) {
+        btn.classList.add("disabled")
+    } else {
+        btn.classList.remove("disabled")
+    }
+}
+
+const renderCartCount = () => {
+    cartCount.innerHTML = cart.reduce((acc, cur) => {
+        return acc + cur.cantidad
+    }, 0)
+}
+
+const updateCartState = () => {
+    // Guardar carrito en LocalStorage
+    saveCart()
+    // Renderizar carrito
+    renderCart()
+    // Mostrar el total del carrito
+    showCartTotal()
+    // Chequear disable de Botones
+    disableButton(buyBtn)
+    // Render Burbuja del cart
+    renderCartCount()
+}
+
+const addProduct = (e) => {
+    if (!e.target.classList.contains("btn-add")) {
+        return
+    }
+    const product = createProductData(e.target.dataset)
+    // Si el producto ya existe
+    if (isExistingCartProduct(product)) {
+        // Agregamos unidad al producto
+        addUnitToProduct(product)
+        // Damos Feedbak
+        showSuccessModal("Se agregó una unidad del producto al carrito", product.id)
+    } else {
+        // Si no existe, creamoe el card de productos
+        // Creamos el nuevo producto en el array
+        createCartProduct(product)
+        // Damos feedback
+        showSuccessModal("El producto se ha agregado al carrito", product.id) 
+    }
+    // Actualizamos data del carrito
+    updateCartState()
+}
+
+const removeProductFromCart = (existingProduct) => {
+    cart = cart.filter((product) => {
+        return product.id !== existingProduct.id
+    })
+    updateCartState()
+}
+
+const substractProductUnit = (existingProduct) => {
+    cart = cart.map((product) => {
+        return product.id === existingProduct.id
+            ? {...product,  cantidad: Number(product.cantidad) - 1}
+            : product
+    })
+}
+
+
+const handleMinusBtnEvent = (id) => {
+    const existingCartProduct = cart.find((item) => item.id === id)
+    if (existingCartProduct.cantidad === 1) {
+        // Eliminar Producto
+        if (window.confirm("¿Desea eliminar el producto del carrito?")) {
+            removeProductFromCart(existingCartProduct)
+        }
+        return
+    }    
+    // Sacarle Unidad al producto
+    substractProductUnit(existingCartProduct)
+}
+
+const handlePlusBtnEvent = (id) => {
+    const existingCartProduct = cart.find((item) => item.id === id)
+    addUnitToProduct(existingCartProduct)
+}
+
+const deleteProduct = (id) => {
+    const existingCartProduct = cart.find((item) => item.id === id)
+    if (window.confirm("¿Desea eliminar el producto del carrito?")) {
+        removeProductFromCart(existingCartProduct)
+    }
+}
+
+const handleQuantity = (e) => {
+    if (e.target.classList.contains("down")) {
+        // Boton -
+        handleMinusBtnEvent(e.target.dataset.id)
+    } else if (e.target.classList.contains("up")){
+        // Boton +
+        handlePlusBtnEvent(e.target.dataset.id)
+    } else if (e.target.classList.contains("item-trash")) {
+        // Boton Delete
+        deleteProduct(e.target.dataset.id)
+        
+    }
+    // Actualizar estado del carrito
+    updateCartState()
+}
 
 const init = () => {
     // Load
     window.addEventListener('load', verificarTamañoPantalla);
+    document.addEventListener("DOMContentLoaded", renderCart)
+    document.addEventListener("DOMContentLoaded", showCartTotal)
     // Resixe
     window.addEventListener('resize', verificarTamañoPantalla);
     // Scroll
@@ -114,6 +392,9 @@ const init = () => {
     // Click
     iconCart.addEventListener('click', toggleCart)
     iconMenu.addEventListener('click', toggleMenu)
+    categoriesContainer.addEventListener('click', applyFilter)
+    productsContainer.addEventListener("click", addProduct)
+    productsCart.addEventListener("click", handleQuantity)
     // window.addEventListener("scroll", closeOnScroll);
     overlay.addEventListener('click', closeOnOverlayClick)
     iconCloseCart.addEventListener("click", closeOnOverlayClick)
@@ -121,28 +402,11 @@ const init = () => {
     //ForEachs
     boxes.forEach((box) => animationBoxes.observe(box))
     // Functions
-    renderProducts()
+    renderFilteredProducts()
+    disableButton(buyBtn)
+    renderCartCount()
+    productModal = document.querySelectorAll(".product-modal")
+    console.log(productModal)
 }
 
 init()
-
-// SPLIDE JS
-const splideProducts = new Splide( '.splide-products', {
-    perPage: '4',
-    type: "loop",
-    gap: "40px",
-    perMove: '1',
-    pagination: false,
-    breakpoints: {
-        1300: {
-            perPage: 3,
-        },
-        1000: {
-            perPage: 2,
-        },
-        700: {
-            perPage: 1,
-        }
-    },
-});
-splideProducts.mount()
